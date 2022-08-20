88cb264db136c938b58f20fab2472c94a23783a9
Edward A Maxedon Sr <edadma@gmail.com>
2022-08-20 18:16:47 -0400

    format parser

--- src/main/scala/io/github/spritzsn/logger/Main.scala
+++ src/main/scala/io/github/spritzsn/logger/Main.scala
@@ -8,6 +8,7 @@ import io.github.spritzsn.fs.*
 import io.github.spritzsn.libuv.*
 
 @main def run(): Unit =
+  println(parse(":method :url :status :res[content-length] - :response-time[2] ms"))
 
 //  async {
 //    val fd = await(open("asdf", O_WRONLY | O_CREAT, S_IRWXU))
--- src/main/scala/io/github/spritzsn/logger/logger.scala
+++ src/main/scala/io/github/spritzsn/logger/logger.scala
@@ -14,27 +14,44 @@ def apply(): RequestHandler =
     }
     HandlerResult.Next
 
-val tokenRegex = """:(?<name>[a-zA-Z]+)(?:\[(?<arg>[a-zA-Z0-9]+)])?""".r
+val tokenRegex = """:(?<name>[a-zA-Z-]+)(?:\[(?<arg>[a-zA-Z0-9-]+)])?""".r
 val tokenMap =
   Map(
     "method" -> Nil,
     "url" -> Nil,
+    "status" -> Nil,
     "date" -> List("iso", "web"),
+    "res" -> null,
+    "response-time" -> List("3", "0", "1", "2"),
   )
 
 def parse(format: String): List[Segment] =
   val buf = new ListBuffer[Segment]
+  var idx = 0
 
   tokenRegex.findAllMatchIn(format) foreach { m =>
+    def add(tok: Segment.Token): Unit =
+      if m.start > idx then buf += Segment.Literal(format.substring(idx, m.start))
+      buf += tok
+      idx = m.end
+
     val name = m.group("name")
     val arg = m.group("arg")
 
     tokenMap get name match
-      case Some(Nil)                        => Segment.Token(name, None)
-      case Some(args) if arg == null        => Segment.Token(name, Some(args.head))
-      case Some(args) if args contains args => Segment.Token(name, Some(arg))
-      case Some(args) => sys.error(s"logger: token '$name' doesn't accept argument '$arg' - can be $args")
+      case Some(null) if arg == null => sys.error(s"logger: token '$name' requires an argument")
+      case Some(null)                => add(Segment.Token(name, Some(arg)))
+      case Some(Nil) if arg != null  => sys.error(s"logger: token '$name' doesn't accept any argument: found $arg")
+      case Some(Nil)                 => add(Segment.Token(name, None))
+      case Some(args) if arg == null => add(Segment.Token(name, Some(args.head)))
+      case Some(args) if args contains arg => add(Segment.Token(name, Some(arg)))
+      case Some(args) =>
+        sys.error(s"logger: token '$name' doesn't accept argument '$arg': can be one of ${args mkString ","}")
+      case None => sys.error(s"logger: token '$name' not recognized")
   }
+
+  if format.length > idx then buf += Segment.Literal(format.substring(idx))
+  buf.toList
 
 enum Segment:
   case Token(name: String, arg: Option[String]) extends Segment
